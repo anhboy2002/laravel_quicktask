2c45d9ed2bb591c91627ca334a2b7feecd084c13
Manh Hung <anhboy2002@gmail.com>
2019-04-09 11:42:01 +0700

    create-migration

---
+++ storage/framework/views/7f6ceaaae09d7cc3831e70b6973744de7d0746d1.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <title>Laravel Quickstart - Intermediate</title>
+
+    <!-- CSS And JavaScript -->
+</head>
+
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <!-- Navbar Contents -->
+    </nav>
+</div>
+
+<?php echo $__env->yieldContent('content'); ?>
+</body>
+</html>
